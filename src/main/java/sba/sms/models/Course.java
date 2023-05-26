7331a31565ae369e564bc7abaff64d34b6766afa
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:29:25 -0400

    Requirement 1 - Models: Course - add getters and setters

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -40,5 +40,37 @@ public class Course {
         this.name = name;
         this.instructor = instructor;
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
 }
