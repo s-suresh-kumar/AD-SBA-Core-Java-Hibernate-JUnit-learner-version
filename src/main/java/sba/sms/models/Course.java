92a21997c4c957b2ae43528e0ab121717f4f6e19
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:26:31 -0400

    Requirement 1 - Models: Course - add all-args constructor

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -29,4 +29,11 @@ public class Course {
     public Course() {
     }
 
+    public Course(int id, String name, String instructor, List<Student> students) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+        this.students = students;
+    }
+    
 }
