cce7a2566b3bd768d4af1d3dbc80bb350bd6c1cf
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:11:11 -0400

    Requirement 1 - Models: Student with table/hibernate annotations

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,29 @@
 package sba.sms.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name="student")
 public class Student {
+    @Id
+    @Column(name="email", nullable=false, length=50)
+    private String email;
+
+    @Column(name="name", nullable=false, length=50)
+    private String name;
+
+    @Column(name="password", nullable=false, length=50)
+    private String password;
+
+    @ManyToMany(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH},fetch=FetchType.EAGER)
+    @JoinTable(name="student_courses",
+            joinColumns = @JoinColumn(name="student_email"),
+            inverseJoinColumns = @JoinColumn(name="course_id"))
+    private List<Course> courses = new ArrayList<>();
 
 }
