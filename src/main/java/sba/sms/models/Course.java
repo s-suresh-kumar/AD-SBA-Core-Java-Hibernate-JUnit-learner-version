e1da1ae4b9d38bdd3d316d2c30a36abb3c95a099
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:24:22 -0400

    Requirement 1 - Models: Course with table/hibernate annotations

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,29 @@
 package sba.sms.models;
 
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name="course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id", nullable=false)
+    private int id;
+
+    @Column(name="name", nullable=false, length=50)
+    private String name;
+
+    @Column(name="instructor", nullable=false, length=50)
+    private String instructor;
+
+    @ManyToMany(mappedBy = "courses", cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH},fetch
+            =FetchType.EAGER)
+    private List<Student> students = new ArrayList<>();
 
 }
