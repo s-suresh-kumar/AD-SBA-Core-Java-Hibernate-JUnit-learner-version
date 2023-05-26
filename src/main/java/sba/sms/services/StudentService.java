9e46a1f0445da5a2470e4e343c7fb6f1c5d596d5
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:39:08 -0400

    Requirement 3- Service Layer: StudentService - implement getAllStudents()

--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,5 +1,37 @@
 package sba.sms.services;
 
-public class StudentService  {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import sba.sms.dao.StudentI;
+import sba.sms.models.Course;
+import sba.sms.models.Student;
+import sba.sms.utils.HibernateUtil;
+
+import java.util.List;
+
+public class StudentService  implements StudentI {
+    @Override
+    public List<Student> getAllStudents(){
+        SessionFactory sessionFactory;
+        Session session = null;
+        List<Student> students = null;
+
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+
+            students =
+                    session.createQuery("FROM Student", Student.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return students;
+    }
 
 }
