a55461dc12ce353fecd942731d124a86d93a0145
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-05-26 17:53:06 -0400

    Requirement 3- Service Layer: CourseService - implement createCourse()

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -1,8 +1,39 @@
 package sba.sms.services;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import sba.sms.dao.CourseI;
+import sba.sms.models.Course;
+import sba.sms.utils.HibernateUtil;
 
+import java.util.List;
 
-public class CourseService  {
+public class CourseService implements CourseI {
+    public void createCourse(Course course){
+        SessionFactory sessionFactory;
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(course);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 
 }
